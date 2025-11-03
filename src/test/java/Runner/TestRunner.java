602d44d16041ee8ee39caff374cd50a5156703e1
Vylor <vylor@vylor.ai>
2025-11-03 13:19:37 +0000

    Integrate Allure reporting into project
    - Added Allure Cucumber adapter to pom.xml
    - Inserted Allure TestNG listener in testng.xml
    - Updated TestRunner.java to include Allure Cucumber7 JVM plugin
    
    This configuration allows for Allure reports to be generated during the Maven verify phase for both TestNG and Cucumber tests.

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/Features/register.feature",
         glue = {"StepDefination","Hooks"},
-        plugin = {"pretty", "html:target/cucumber-report.html"},
+        plugin = {"pretty", "html:target/cucumber-report.html", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
         monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
