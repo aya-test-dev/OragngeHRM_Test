f31026ddb5a5a83d09a25356f29d8bcb5fe1216b
Vylor <vylor@vylor.ai>
2025-11-10 23:56:00 +0000

    Fix: Replace absolute file path with classpath resource loading in ContactUsFormPageTest.java and add test PDF to resources.

--- src/test/java/TestPages/ContactUsFormPageTest.java
+++ src/test/java/TestPages/ContactUsFormPageTest.java
@@ -4,13 +4,16 @@ import Pages.ContactUsFormPage;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 import utils.BaseTest;
+import java.net.URL;
+import java.nio.file.Paths;
+import org.testng.Assert;
 
 public class ContactUsFormPageTest extends BaseTest {
 
     ContactUsFormPage obj;
 
     @Test
-    public void checkContactForm(){
+    public void checkContactForm() throws Exception {
 
         obj = new ContactUsFormPage(driver);
 
@@ -23,7 +26,10 @@ public class ContactUsFormPageTest extends BaseTest {
         obj.setEmailContact("ayaali@gmail.com");
         obj.setSubjectContact("bla bla");
         obj.setMessageContact("brjhbvejhvbejhbvjbvjhbvjhbjh");
-        obj.setFileContact("C:/Users/Aya Ali/OneDrive/Desktop/Ahmed-Ali final-cv.pdf");
+        URL resource = getClass().getClassLoader().getResource("Ahmed-Ali final-cv.pdf");
+        Assert.assertNotNull(resource, "Test resource PDF not found");
+        String filePath = Paths.get(resource.toURI()).toString();
+        obj.setFileContact(filePath);
 
         obj.clickSubmitButton();
 
